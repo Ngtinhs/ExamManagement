87ae2a960933145640c25ebb9b00a83074e209e9
Ngtinhs <nguyenvanductinh77@gmail.com>
2022-11-25 09:52:51 +0700

    [3] Dieu huong trang

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-container">
       <Header />
+      test Link
+      <div>
+        <button>
+          <Link to="/users">Go to user page</Link>
+        </button>
+        <button>
+          <Link to="/admin">Go to admin page</Link>
+        </button>
+      </div>
     </div>
   );
 }
