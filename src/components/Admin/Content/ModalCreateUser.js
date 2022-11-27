726d45ea33cec0db65eae830a2d91ea6c441edd6
Ngtinhs <nguyenvanductinh77@gmail.com>
2022-11-27 12:27:12 +0700

    [15] Customize axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,9 +1,9 @@
-import axios from "axios";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
 import { toast } from "react-toastify";
+import { postCreateNewUser } from "../../../services/apiServices";
 
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
@@ -54,24 +54,22 @@ const ModalCreateUser = (props) => {
       return;
     }
 
-    //call apis
-    const data = new FormData();
-    data.append("email", email);
-    data.append("password", password);
-    data.append("username", username);
-    data.append("role", role);
-    data.append("userImage", image);
+    let data = await postCreateNewUser(email, password, username, role, image);
 
-    const res = await axios.post(
-      "http://localhost:8081/api/v1/participant",
-      data
-    );
-    if (res.data && res.data === 0) {
-      toast.success(res.data.EM);
+    // if (res.data && res.data.EC === 0) {
+    //   toast.success(res.data.EM);
+    //   handleClose();
+    // }
+    // if (res.data && res.data.EC !== 0) {
+    //   toast.error(res.data.EM);
+    // }
+
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
       handleClose();
     }
-    if (res.data && res.data !== 0) {
-      toast.error(res.data.EM);
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
     }
   };
   return (
