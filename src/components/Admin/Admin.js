9331b99b79a1bebfe993b2070b6867a49c3227cf
Ngtinhs <nguyenvanductinh77@gmail.com>
2022-11-27 10:45:03 +0700

    [14] Validate data và React Toastify

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,6 +6,9 @@ import './Admin.scss';
 import {
     Outlet
 } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Admin = (props) => {
@@ -25,6 +28,21 @@ const Admin = (props) => {
                     <Outlet />
                 </div>
             </div>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                />
+                {/* Same as */}
+            <ToastContainer />
         </div>
     )
 }
