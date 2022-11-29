dbd86b20f80f55b69965cd9807e795db2f5114f7
Ngtinhs <nguyenvanductinh77@gmail.com>
2022-11-29 17:29:00 +0700

    [16] Hien thi danh sach user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,34 +1,35 @@
-import ModalCreateUser from "./ModalCreateUser"
-import './ManageUser.scss'
-import { FcPlus } from 'react-icons/fc'
-import { useState } from "react"
+import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
+import { FcPlus } from "react-icons/fc";
+import { useState } from "react";
+import TableUser from "./TableUser";
 
 const ManageUser = (props) => {
-    const [showModalCreateUser, SetshowModalCreateUser] = useState("false")
-    return (
-        <div className='manag-user-container'>
-            <div className='title'>
-                Manage User
-            </div>
-            <div className='users-content'>
-                <div className="btn-add-new">
-                    <button className="btn btn-primary" onClick={() => SetshowModalCreateUser(true)}>
-                    <FcPlus />
-                    Add new user</button>
-                </div>
-
-                <div className="table-users-container">
-                    table users
-                </div>
-                <ModalCreateUser show={showModalCreateUser}
-                setShow ={SetshowModalCreateUser}
-                />
-
-            </div>
-
-
+  const [showModalCreateUser, SetshowModalCreateUser] = useState("false");
+  return (
+    <div className="manag-user-container">
+      <div className="title">Manage User</div>
+      <div className="users-content">
+        <div className="btn-add-new">
+          <button
+            className="btn btn-primary"
+            onClick={() => SetshowModalCreateUser(true)}
+          >
+            <FcPlus />
+            Add new user
+          </button>
         </div>
-    )
-}
 
-export default ManageUser
+        <div className="table-users-container">
+          <TableUser />
+        </div>
+        <ModalCreateUser
+          show={showModalCreateUser}
+          setShow={SetshowModalCreateUser}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ManageUser;
